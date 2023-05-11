e06f4092b3cde4b169afa43726915606f4651f2d
Andrew Sokolov <artas90@gmail.com>
2023-05-11 17:56:35 +0300

    update nvim

--- nvim/build.sh
+++ nvim/build.sh
@@ -1,46 +1,52 @@
 #!/bin/sh
 
+VERSION=`cat VERSION | xargs`
+UBUNTU_REL=`cat UBUNTU_REL | xargs`
+
+# VERSION="0.9.0"
+# UBUNTU_REL="focal"
+
+mkdir -p /tmp/app && cd /tmp/app
+
 # -- install deps -- -- --
 
 apt update
 apt install -y wget git apt-transport-https
 
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
-echo 'deb https://apt.kitware.com/ubuntu/ xenial main' | tee /etc/apt/sources.list.d/kitware.list
+echo "deb https://apt.kitware.com/ubuntu/ $UBUNTU_REL main" | tee /etc/apt/sources.list.d/kitware.list
 
 apt update
 apt install -y build-essential cmake unzip pkg-config gettext libtool-bin
 
 # -- get source -- -- --
 
-VERSION=`cat VERSION | xargs`
-
-pkgname="nvim_"$VERSION"_amd64"
+pkgname="nvim-"$VERSION"."$UBUNTU_REL".amd64"
 
 url="https://github.com/neovim/neovim/archive/refs/tags/v$VERSION.tar.gz"
 wget -q --show-progress -O nvim.tgz "$url"
 
 mkdir -p $pkgname/DEBIAN $pkgname/usr/local
 tar -xvf "nvim.tgz" --no-same-owner
-rm nvim.tgz
+# rm nvim.tgz
 
 # -- build -- -- --
 
 cd "neovim-$VERSION"
 export CMAKE_BUILD_TYPE="RelWithDebInfo"
-export CMAKE_INSTALL_PREFIX="/app/$pkgname/usr/local"
-export CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/app/$pkgname/usr/local"
+export CMAKE_INSTALL_PREFIX="/tmp/app/$pkgname/usr/local"
+export CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/tmp/app/$pkgname/usr/local"
 
 make
 
-# ugly fix
+# ugly fix for Ubuntu 16.04
 fixfile="cmake.deps/cmake/DownloadAndExtractFile.cmake"
 sed -i 's#${CMAKE_COMMAND} -E tar xfz#tar --no-same-owner --no-overwrite-dir --touch -xzf#' $fixfile
 
 make
 make install
 
-rm -rf "neovim-$VERSION"
+# rm -rf "neovim-$VERSION"
 
 cd ..
 
@@ -55,7 +61,9 @@ Description: Vim-fork focused on extensibility and usability
 EOF
 
 dpkg-deb --build $pkgname
-rm -rf $pkgname
+# rm -rf $pkgname
+
+cp /tmp/app/$pkgname.deb /app/$pkgname.deb
 
 if [ -n "$GITHUB_ENV" ]; then
   echo "PKG_TAG=$pkgname"           >> $GITHUB_ENV
